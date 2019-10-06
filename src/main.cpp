e9d2b9a0d02f5a2b18eb6ebc96860fbce7b734f2
Diego Paiva <diego.paiva13@gmail.com>
2019-10-06 19:39:33 -0300

    Added src folder

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file    main.cpp
+ * @author  Diego Paiva e Silva
+ * @date    24/09/2019
+ */
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+  printf("Hello World!");
+  return 0;
+}
